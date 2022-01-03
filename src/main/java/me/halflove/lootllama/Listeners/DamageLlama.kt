f070546222b5bc3eb8a5597bcb46719c4b10b1de
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 12:08:25 -0700

    Added more listeners to fix a few issues and polish up the events

--- src/main/java/me/halflove/lootllama/Listeners/DamageLlama.kt
+++ src/main/java/me/halflove/lootllama/Listeners/DamageLlama.kt
@@ -1,11 +1,15 @@
 package me.halflove.lootllama.Listeners
 
 import me.halflove.lootllama.Misc.DropItem
+import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Llama
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.player.PlayerInteractEntityEvent
 
 /*
 *
@@ -25,6 +29,20 @@ class DamageLlama: Listener {
                 event.damage = 1.0
                 DropItem.itemDrop(event.entity.location)
             }
+        }
+    }
+
+    @EventHandler
+    fun feedLlama(event: PlayerInteractEntityEvent){
+        if(event.rightClicked.type == EntityType.LLAMA && event.player.location.world?.name == "world") {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun deathLlama(event: EntityDeathEvent) {
+        if(event.entityType == EntityType.LLAMA && event.entity.location.world?.name == "world") {
+            event.drops.clear()
         }
     }
 
