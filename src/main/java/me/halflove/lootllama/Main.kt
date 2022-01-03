4c40974daf44651c29a5eb5b6508f600e3b4253f
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 12:07:30 -0700

    Initialize storage and loot table generation

--- src/main/java/me/halflove/lootllama/Main.kt
+++ src/main/java/me/halflove/lootllama/Main.kt
@@ -2,6 +2,8 @@ package me.halflove.lootllama
 
 import me.halflove.lootllama.Commands.LlamaEvent
 import me.halflove.lootllama.Listeners.DamageLlama
+import me.halflove.lootllama.Misc.DropItem
+import me.halflove.lootllama.Misc.Storage
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -19,5 +21,7 @@ class Main: JavaPlugin() {
     override fun onEnable(){
         getCommand("lootllama")?.setExecutor(LlamaEvent())
         Bukkit.getPluginManager().registerEvents(DamageLlama(), this)
+        Storage.createDataFile(this)
+        DropItem.generateLootTable()
     }
 }
