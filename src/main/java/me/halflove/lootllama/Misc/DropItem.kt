18b20c2206317925dad983ccf4c20b19aecdb1cf
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 12:08:05 -0700

    Finished loot generation

--- src/main/java/me/halflove/lootllama/Misc/DropItem.kt
+++ src/main/java/me/halflove/lootllama/Misc/DropItem.kt
@@ -1,9 +1,10 @@
 package me.halflove.lootllama.Misc
 
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.inventory.ItemStack
-import java.util.*
 
 /*
 *
@@ -16,16 +17,37 @@ import java.util.*
 
 object DropItem {
 
-    fun itemDrop(loc: Location) {
-        val randomNumber = (0..2).random()
-        if(randomNumber == 0){
-            successfulDrop(loc)
+    var lootTableSize = 0
+
+    fun generateLootTable(){
+        val contentsSection: ConfigurationSection? = Storage.data.getConfigurationSection("loot")
+        if (contentsSection != null) {
+            for(lootTableSlot in contentsSection.getKeys(false)) {
+                Bukkit.broadcastMessage("" + lootTableSlot.toInt())
+                lootTableSize += 1
+            }
         }
     }
 
-    private fun successfulDrop(loc: Location) {
-        val item = ItemStack(Material.DIAMOND)
+    fun itemDrop(loc: Location) {
+        val randomNumber = (0..2).random()
+        if(randomNumber == 0){
+            val randomNumber2 = (1..lootTableSize).random()
+            successfulDrop(loc, getSlotItemStack(randomNumber2))
+            Bukkit.broadcastMessage("$randomNumber2")
+        }
+    }
+
+    private fun successfulDrop(loc: Location, item: ItemStack) {
         loc.world?.dropItemNaturally(loc, item)
+    }
+
+    //Builds and returns the requested item (slot) stored in the data.yml
+    private fun getSlotItemStack(slot: Int): ItemStack {
+        return ItemStack(
+            Material.valueOf(Storage.data.get("loot.$slot.type").toString()),
+            Storage.data.get("loot.$slot.amount") as Int
+        )
     }
 
 }
